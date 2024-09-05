74797902b1de06c663a1b3f9ddd08f3114fe1692
oulibouli <oulikhanow.nico@gmail.com>
2024-09-05 15:21:15 +0200

    Create banner and home template

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { BannerComponent } from './pages/banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent, BannerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
